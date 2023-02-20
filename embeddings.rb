0a58370a8ddcbabef1a584c806d622b08461ea58
Kane Hooper <kanehooper@hotmail.com>
2023-02-20 15:40:42 +1100

    Remove API key

--- embeddings.rb
+++ embeddings.rb
@@ -1,7 +1,7 @@
 require 'ruby/openai'
 require 'csv'
 
-client = OpenAI::Client.new(access_token: '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')
+client = OpenAI::Client.new(access_token: '<YOUR_API_KEY>')
 
 text_array = []
 embedding_array = []
